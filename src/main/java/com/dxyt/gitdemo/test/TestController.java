005a974c17e5142df61109dfbdcd2aad56c7595f
18510523772 <ahfemail@163.com>
2018-09-29 13:49:20 +0800

    新建testController类

---
+++ src/main/java/com/dxyt/gitdemo/test/TestController.java
@@ -0,0 +1,16 @@
+package com.dxyt.gitdemo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/testGit")
+    public String testGit(){
+        System.out.println("安红飞");
+
+        return "";
+    }
+
+}
