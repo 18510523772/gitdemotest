555a70147f90f87d2c6bd4f23525738c60fc54e2
18510523772 <ahfemail@163.com>
2018-09-29 14:02:57 +0800

    测试冲突

--- src/main/java/com/dxyt/gitdemo/test/TestController.java
+++ src/main/java/com/dxyt/gitdemo/test/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
     @RequestMapping("/testGit")
     public String testGit(){
         System.out.println("安红飞");
-
+        System.out.println("dfdf");
         return "";
     }
 
