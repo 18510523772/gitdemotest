c284417eb3252fb5d5f12d66f20ccaeab64f0aa1
18510523772 <ahfemail@163.com>
2018-09-29 12:16:47 +0800

    初始提交项目

---
+++ src/main/java/com/dxyt/gitdemo/GitdemoApplication.java
@@ -0,0 +1,12 @@
+package com.dxyt.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitdemoApplication.class, args);
+    }
+}
