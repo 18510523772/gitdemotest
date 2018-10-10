d5bbc444ebdcf755a960189182a278b9b7e07ec0
18510523772 <ahfemail@163.com>
2018-10-10 15:43:36 +0800

    gggggggg

---
+++ src/main/java/com/dxyt/gitdemo/test/An.java
@@ -0,0 +1,16 @@
+package com.dxyt.gitdemo.test;
+
+/**
+ * 项目名称：gitdemo
+ * 类名称：An
+ * 类描述：
+ * 创建人：安红飞（anhongfei@telecomyt.com.cn）
+ * 创建时间：2018/10/10 12:51
+ * 修改人：安红飞（anhongfei@telecomyt.com.cn）
+ * 修改时间：2018/10/10 12:51
+ * 修改备注：
+ * 版本号：1.0
+ **/
+public class An {
+
+}
---
+++ src/main/java/com/dxyt/gitdemo/test/AnInterface.java
@@ -0,0 +1,15 @@
+package com.dxyt.gitdemo.test;
+
+/**
+ * 项目名称：gitdemo
+ * 接口名称：AnInterface
+ * 接口描述：
+ * 创建人：安红飞（anhongfei@telecomyt.com.cn）
+ * 创建时间：2018/10/10 12:52
+ * 修改人：安红飞（anhongfei@telecomyt.com.cn）
+ * 修改时间：2018/10/10 12:52
+ * 修改备注：
+ * 版本号：1.0
+ **/
+public interface AnInterface {
+}
