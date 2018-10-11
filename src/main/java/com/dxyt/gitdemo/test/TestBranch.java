a4dd7ec58ca1c6e19d6d3940e959710665653c45
18510523772 <ahfemail@163.com>
2018-10-11 16:56:29 +0800

    11111111

---
+++ src/main/java/com/dxyt/gitdemo/test/TestBranch.java
@@ -0,0 +1,15 @@
+package com.dxyt.gitdemo.test;
+
+/**
+ * 项目名称：gitdemo
+ * 类名称：TestBranch
+ * 类描述：
+ * 创建人：安红飞（anhongfei@telecomyt.com.cn）
+ * 创建时间：2018/10/11 16:33
+ * 修改人：安红飞（anhongfei@telecomyt.com.cn）
+ * 修改时间：2018/10/11 16:33
+ * 修改备注：
+ * 版本号：1.0
+ **/
+public class TestBranch {
+}
