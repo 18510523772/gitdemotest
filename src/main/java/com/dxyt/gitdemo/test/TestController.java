f8c9dafeba4332d51c421020d338511564c8cfd0
18510523772 <ahfemail@163.com>
2018-10-11 13:29:47 +0800

    iiiiiiiiiiiiiii

--- src/main/java/com/dxyt/gitdemo/test/TestController.java
+++ src/main/java/com/dxyt/gitdemo/test/TestController.java
@@ -3,6 +3,8 @@ package com.dxyt.gitdemo.test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 @Controller
 public class TestController {
     
@@ -55,6 +57,24 @@ public class TestController {
         System.out.println("rtretger");
         System.out.println("rtretger");
         return "";
+    }
+
+    /**
+     * 方法名称：testAn
+     * 方法描述：
+     * 创建人：安红飞（anhongfei@telecomyt.com.cn）
+     * 创建时间：2018/10/11 13:25
+     * 修改人：安红飞（anhongfei@telecomyt.com.cn）
+     * 修改时间：2018/10/11 13:25
+     * 修改备注：
+     * 入参: str
+     * 入参: id
+     * 入参: date
+     * 返回值类型： java.lang.Integer
+     **/
+    public Integer testAn(String str, Integer id, Date date){
+
+        return null;
     }
 
 }
