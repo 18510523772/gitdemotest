b5beb6f12475b7e2add5d2c656c67830db61afd6
18510523772 <ahfemail@163.com>
2018-10-12 09:27:20 +0800

    2222222

--- src/main/java/com/dxyt/gitdemo/test/TestBranch.java
+++ src/main/java/com/dxyt/gitdemo/test/TestBranch.java
@@ -12,4 +12,22 @@ package com.dxyt.gitdemo.test;
  * 版本号：1.0
  **/
 public class TestBranch {
+
+    /**
+     * 方法名称：addBranch
+     * 方法描述：
+     * 创建人：安红飞（anhongfei@telecomyt.com.cn）
+     * 创建时间：2018/10/12 9:09
+     * 修改人：安红飞（anhongfei@telecomyt.com.cn）
+     * 修改时间：2018/10/12 9:09
+     * 修改备注：
+     * 入参: id
+     * 入参: userName
+     * 入参: password
+     * 返回值类型： int
+     **/
+    public int addBranch(Integer id,String userName,String password) {
+
+        return 0;
+    }
 }
